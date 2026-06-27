20e1701576e94229ee92c12f12ccd79abdfbcd74
Sopln-ishan <23ishansekhar10d@gmail.com>
2026-06-27 19:41:02 +0530

    changed some code to do cool stuff

--- backend/src/controllers/auth.controllers.ts
+++ backend/src/controllers/auth.controllers.ts
@@ -11,6 +11,9 @@ export const signup = async (req: Request, res: Response) => {
       res.status(400).json({ message: "All fields are required" });
     }
 
+    const name = fullName.trim();
+    const normalizedEmail = email.trim().toLowerCase();
+
     if (password.length < 6) {
       res
         .status(400)
@@ -32,8 +35,8 @@ export const signup = async (req: Request, res: Response) => {
     const newUser = new User({ fullName, email, password: hashedPassword });
 
     if (newUser) {
-      generateToken(newUser._id, res);
-      await newUser.save();
+      const savedUser = await newUser.save();
+      generateToken(savedUser._id, res);
 
       res.status(201).json({
         _id: newUser._id,
